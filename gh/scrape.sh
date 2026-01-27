9df869d721f47cfca39af2f298e1da036b2c2062
Chris McKenzie <kristopolous@yahoo.com>
2026-01-26 16:56:26 -0800

    updating the scrape

--- gh/scrape.sh
+++ gh/scrape.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[[ -d servers ]] || git clone https://github.com/modelcontextprotocol/servers
 grep -Po 'https://github.com/[^?#"\s)]*' servers/README.md | grep -E 'https://github[^)]*/[^)]*/[^)]*' | cut -c 20- | cut -d '/' -f 1,2 | sort | uniq | while read i; do
     echo -e "\n\n$i"
     if [[ ! -e "$i" ]]; then
