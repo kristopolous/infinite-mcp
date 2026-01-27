a55b86d33b3f701f8247f4af4ef4cff6c5cb8ce8
Chris McKenzie <kristopolous@yahoo.com>
2026-01-26 16:54:24 -0800

    adding the gh scraper

---
+++ gh/scrape.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+grep -Po 'https://github.com/[^?#"\s)]*' servers/README.md | grep -E 'https://github[^)]*/[^)]*/[^)]*' | cut -c 20- | cut -d '/' -f 1,2 | sort | uniq | while read i; do
+    echo -e "\n\n$i"
+    if [[ ! -e "$i" ]]; then
+        base="$(dirname $i)";
+        [[ -d "$base" ]] || mkdir "$base"
+        timeout 10s git clone --depth 1 "https://github.com/$i" "$i"
+    else
+        (
+            cd "$i"
+            what=$(git rev-parse --abbrev-ref HEAD)
+            timeout 5s git fetch origin $what
+            git reset --hard origin/$what
+        )
+    fi
+done
